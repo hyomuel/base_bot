f72410a3bb543463fce1c1b5661f49125bd5ccb3
효무엘 <help@hyomuel.com>
2025-07-18 23:59:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ class Hyomuel_Bot(commands.Bot):
 	async def on_ready(self):
 		print(f"> {self.user} 로그인")
 
+def remove_pycache(target_dir): # __pycache__가 있는게 너무 신경쓰여서 만든 함수
+    for root, dirs, files in os.walk(target_dir, topdown=False):
+        for dir in dirs:
+            if dir == "__pycache__":
+                shutil.rmtree(os.path.join(root, dir))
+    print("> 캐시 삭제 완료")
+
 
 bot = Hyomuel_Bot()
+
+for file in os.listdir("cogs"):
+    if file.endswith(".py"):
+        if file.startswith("_"):
+            continue
+        bot.load_extension(f"cogs.{file[:-3]}") # cogs 폴더에서 파일 이름 앞에 _가 없을 경우에만 적용시키기
+
+remove_pycache("./")
 bot.run(token)
+
